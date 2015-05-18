2c22d4020dc9be186cdf6e6edcc0c068c02ce4d4
Radek Adamus <adamus.r@gmail.com>
2015-05-18 14:43:29 +0200

    githubarchiverstreamer comandline arguments implemented

--- githubarchivestreamer.js
+++ githubarchivestreamer.js
@@ -3,90 +3,95 @@ var fs = require('fs');
 
 (function(){
 
-	var months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-	var days = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-	var hours = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+	var months = {table:["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]};
+	var days = {table:["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]};
+	var hours = {table:["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]};
 	
 	
+	var createIndex = function(dateobj) {
+		dateobj.idx = {};		
+		dateobj.table.forEach(function(elem){ dateobj.idx[parseInt(elem)] = elem; });		
+	}
 	
-	var url = "http://data.githubarchive.org/";
+	createIndex(months); 
+	createIndex(days);
+	createIndex(hours);
+		
+	var error = function(info){
+		console.log(info); 
+		process.exit();
+	}
+	var getDateElement = function(elem, dateobj, name){
+		var result;
+		if(elem == "*"){
+			result = dateobj.table;
+		}
+		else if(isNaN(elem)){
+			var range = elem.split("-");
+			if(range.length == 2){
+				var min = parseInt(range[0]);
+				var max = parseInt(range[1]);
+				if(isNaN(min) || isNaN(max) || min > max){
+					error("require * or a number or a range for "+name + " has: " + elem);
+				}
+				max = max > dateobj.table.length ? dateobj.table.length : max;   
+				result = [];
+				for(var i = min; i <= max; i++){
+					result.push(dateobj.idx[i]);
+				}
+			}else {
+				error("require * or a number or a range for "+name + " has: " + elem); 				
+			}
+		}else {
+			result = [dateobj.idx[parseInt(elem)]];
+		}
+		return result;
+	}
+	
+	var path = "http://data.githubarchive.org/";
 	
 	var args = process.argv.slice(2);
 	
 	console.log(JSON.stringify(args));
 	
-	var downloadFile = function(url, filename, onComplete) {
-		var fullURL = url + filename;
-		console.log("downloading " + fullURL);
-		var file = fs.createWriteStream(filename);
-		onComplete = onComplete || function(){};	
-		var request = http.get(fullURL, function(response) {
-				response.pipe(file); 
-				file.on('finish', function() {
-					file.close(function(){onComplete();});
-					
-				});
-		}).on('error', function(e){ console.log("error: " + e); });
-	};
+	
 	
 	var year = [parseInt(args[0])];
 	if(isNaN(year[0])) {console.log("year NaN"); process.exit();}
 	
-	var months;
-	if(args[1] != "*")
-	{	
-		months = [args[1]];
-	}
-	console.log(months);
-	var days;
-	if(args[2] != "*")
-	{	
-		days = [args[2]];
-	}
-	console.log(days);
+	var month = getDateElement(args[1], months, "month");
 	
-	var hours;
+	console.log(month);
+	var day = getDateElement(args[2], days, "day");
+	
+	console.log(day);
+	
 	
 	args[3] = args[3] || "*";
-	if(args[3] != "*")
-	{	
-		hours = [args[3]];
-	}
+	var hour = getDateElement(args[3], hours, "hour");
 	
 	
-	var filenameprefix = year + "-";
 	
-	var download = function(mi, di, hi){
-		if(hi === hours.length){
-			download(mi, di + 1, 0);
-		}else if(di === days.length){
-			download(mi + 1, 0, 0);
-		} else if(mi == months.length){
-			return;
-		}else{
-				var filename = filenameprefix + months[mi] + "-" + days[di] + "-" + hours[hi]+ ".json.gz";		
-				downloadFile(url, filename, function() { download(mi,di, hi + 1);});
-		}
-		
-	}
 	
-	download(0,0,0);
-	/*months.forEach(function(month){
-		var monthname = filenameprefix + month + "-";
-		days.forEach(function(day){
-			var dayname = monthname += day + "-";
-			hours.forEach(function(hour){
-				var filename = dayname + hour + ".json.gz"; 
-				var fullUrl = url + filename;
-				console.log(fullUrl);
-				
-				downloadFile(fullUrl, filename);
-				
-				
+	
+	var prefix = path + year;
+	var suffix = ".json.gz";
+	var sources = [];
+	month.forEach(
+			function(m){
+				var mfile = prefix + "-" + m;
+				day.forEach(
+						function(d){
+							var dfile = mfile + "-" + d;
+							hour.forEach(function(h){
+								var hfile = dfile + "-" + h + suffix;
+								sources.push(hfile);
+								console.log(hfile);
+							});
+						});				
 			});
-			
-		});
-	});*/
+	
+
 
 	
 })()
